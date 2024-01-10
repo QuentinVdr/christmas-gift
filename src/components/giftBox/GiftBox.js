c7e890e4aa0ddbfbe37af54d87803467f713d8f8
Quentin VERDIER <quent1.verdier@gmail.com>
2024-01-10 21:35:33 +0100

    Add images to GiftBox component

--- src/components/giftBox/GiftBox.js
+++ src/components/giftBox/GiftBox.js
@@ -3,6 +3,24 @@ import styles from './GiftBox.module.css';
 export default function GiftBox({ isOpen }) {
   return (
     <div className={isOpen ? `${styles.boxBody} ${styles.boxBodyOpen}` : styles.boxBody}>
+      <div className={styles.images}>
+        <img
+          id={styles.circuit}
+          className={styles.image}
+          src="https://via.placeholder.com/150"
+          alt="cadeau - image circuit"
+          width={150}
+          height={150}
+        />
+        <img
+          id={styles.alpine}
+          className={styles.image}
+          src="https://via.placeholder.com/150"
+          alt="cadeau - image alpine"
+          width={150}
+          height={150}
+        />
+      </div>
       <div className={styles.boxLid}>
         <div className={styles.boxBowtie}></div>
       </div>
